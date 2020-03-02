f5711ab0f791562811cf5e28b49ee4c0b49fa53d
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2020-03-02 11:12:37 +0000

    remove windows specific code

--- config/env.rb
+++ config/env.rb
@@ -1,22 +1,8 @@
 path = File.expand_path '../../', __FILE__
 APP = "sinforum"
 
-windows_dev_only = RUBY_PLATFORM =~ /win32|mingw/
-bundler_require = !windows_dev_only
-
 require "bundler/setup"
-if bundler_require
-  Bundler.require :default
-else
-  puts "loading dev gems without bundler..."
-  File.open("#{path}/Gemfile").each_line do |line|
-    gem_match = line.match /^\s*gem\s*["']([-\w]+)["']/
-    if gem_match
-      break if gem_match[1] =~ /rspec/
-      require gem_match[1]
-    end
-  end
-end
+Bundler.require :default
 
 module Utils
   def require_all(path)
